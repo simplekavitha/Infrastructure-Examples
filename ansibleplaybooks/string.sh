a20e944d16046ba385d09095a82293b7a6978d39
simple <simple.kalleti@gmail.com>
2021-05-13 17:06:11 +0530

    uploading the ansible files

---
+++ ansibleplaybooks/string.sh
@@ -0,0 +1,13 @@
+data1=20
+data2=10
+data3=40
+data4=50
+if [ $data1 -lt $data2 ]
+then
+echo "data1 is less than data2"
+elif [ $data4 -gt $data3 ]
+then
+	echo condition is true
+echo "data1 is greater than data2"
+fi
+
